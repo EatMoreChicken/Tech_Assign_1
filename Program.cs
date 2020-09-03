ed0914eeb5772e710e661afbfd0457109d207239
Varghese Charly <vcharly@usf.edu>
2020-09-03 14:18:43 -0400

    This commit is testing changes in "Program.cs"

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace Tech_Assign_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Testing stuff
+            Console.WriteLine("Testing chanegs");
+
+            // Testing if commits are working correctly
+            Console.WriteLine("This one will test for commit changes!")
         }
     }
 }
